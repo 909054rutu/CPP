65fa520bb9eefbb4aa68be2220267cceff578b0b
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-02-09 17:42:35 +0530

    Add files via upload

---
+++ cal_by_value.cpp
@@ -0,0 +1,25 @@
+  #include<iostream>
+  #include<string.h>
+  using namespace std;
+  void swap (int a,int b)
+  {
+                 int t=a;
+                 a=b;
+                 b=t;
+                 cout<<"\n swap value of a="<<a;
+                 cout<<"\n swap value of b="<<b;
+  }
+  int main()
+  {
+                 int a,b;
+                 cout<<"\n enter 2 numbers";
+                 cin>>a>>b;
+                 cout<<"\nbefore swap a="<<a;
+                  cout<<"\nbefore swap b="<<b;
+                  swap(a,b);
+                  cout<<"\nafter swap a="<<a;
+                  cout<<"\nafter swap b="<<b;
+                  
+                 
+  }
+  
