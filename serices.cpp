dbec04ba7e1b01e6bd66c64ecb130a31e0d4e428
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-02-21 21:01:02 +0530

    Add files via upload

---
+++ serices.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int n, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        sum = sum+i*i;
+    }
+
+    cout << sum;
+
+    return 0;
+}
