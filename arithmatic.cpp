817a295e034decff3deeaf4db1297dd64b0c385e
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-03-06 15:11:24 +0530

    Add files via upload

---
+++ arithmatic.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,ch,c;
+
+cout<<"Enter two numbers:";
+cin>>a>>b;
+cout<<"1 /n 2 /n 3 /n 4./ Enter choice";
+cin>>ch;
+switch(ch)
+{
+case 1:c=a+b;
+       cout<<"Addition="<<c;
+       break;
+case 2:c=a-b;
+       cout<<"Substraction="<<c;
+       break;
+case 3:c=a*b;
+       cout<<"Multiplication="<<c;
+       break;
+case 4:c=a/b;
+       cout<<"Division="<<c;
+       break;
+default: cout<<"Invalid choice:";
+	break;
+}
+
+}
