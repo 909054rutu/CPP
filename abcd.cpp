a35108ae7ce2c02e94ebd544bcecd4146500aeb9
Rutuja Shelke <129090371+909054rutu@users.noreply.github.com>
2024-03-06 15:34:37 +0530

    Add files via upload

---
+++ abcd.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class pattern
+{
+               public:
+               char c ='A';
+                              void disp()
+                              {
+                                             for(char i='A';i<='D';i++)
+                                             {
+                                                            for( char j='A';j<=i;j++)
+                                             
+                                                            cout<<c++;
+                                             {
+                                                            cout<<"\n";
+                                             }
+                              }
+               }
+};
+int main()
+{
+               pattern ob;
+              
+               ob.disp();
+}
